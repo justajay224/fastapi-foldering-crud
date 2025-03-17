0bb49da1ae6f2111e95a6c20491d069d6fd8e0ae
justajay224 <ajaymaulana2224@gmail.com>
2025-03-17 11:23:07 +0700

    main update

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ from route.index import include_routers
 
 app = FastAPI()
 
-# Database setup 
 models.Base.metadata.create_all(bind=engine)
 
 include_routers(app)
 
-# Home route
-# @app.get("/")
-# def home():
-#     return {"message": "FastAPI CRUD Produk"}
